25915fca51f9783f784a13705954ef2f405e06dd
Joao Braganca <joao.paul@braganca.name>
2011-05-28 16:17:19 -0700

    dead simple json response

---
+++ libs/json_nugget_response.php
@@ -0,0 +1,12 @@
+<?php
+App::import('Libs', 'NuggetResponse');
+class JsonNuggetResponse extends NuggetResponse {
+    function  __construct(NuggetController $nugget) {
+        parent::__construct($nugget);
+        $this->content_type = 'application/json';
+        $this->renderCallback = function($model) use($nugget) {
+            echo json_decode($model, true);
+        };
+    }
+}
+?>
