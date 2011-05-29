42f8fa2bada18bf7c037daf677035ed287b3a1c4
Joao Braganca <joao.paul@braganca.name>
2011-05-29 12:01:26 -0700

    really basic implementation

--- controllers/components/content_negotiation_component.php
+++ controllers/components/content_negotiation_component.php
@@ -2,11 +2,28 @@
 App::import('Controller', 'Nugget.Nugget');
 class ContentNegotiationComponent extends Object {
     private $nugget;
+    private $inserted_into_pipeline = false;
+    private $responses = array();
 
     function initialize(NuggetController &$nugget, $settings = array()) {
         $this->nugget = $nugget;
     }
 
-    
+    function respond_as($content_type, $callback) {
+        $this->responses[$content_type] = $callback;
+
+        if ($this->inserted_into_pipeline === true) {
+            return;
+        }
+
+        $responses = $this->responses;
+
+        $this->nugget->post_request->last(function(&$request, &$response) use ($responses){
+            while (count($responses) > 0) {
+                $callback = array_pop($responses);
+                $callback($request, $response);
+            }
+        });
+    }
 }
 ?>
--- features/support/conneg_nugget_controller.php
+++ features/support/conneg_nugget_controller.php
@@ -4,6 +4,13 @@ App::import('Component', 'Nugget.ConentNegotiationComponent');
 class ConnegNuggetController extends NuggetController {
     function  __construct() {
         $this->get['/simple'] = function($request) {
+            $cn = $request->nugget->ContentNegotiation;
+            $cn->respond_as('application/json', function(&$request, &$response) {
+                $json = new JsonNuggetResponse($request->nugget);
+                $json->model = $response->model;
+                $json->code = $response->code;
+                $response = $json;
+            });
             return array('hello' => 'world');
         };
         parent::__construct();
