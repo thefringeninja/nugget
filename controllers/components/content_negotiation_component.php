11b67a55cdf648b0221b1d868c13e8ba41b91b93
Joao Braganca <joao.paul@braganca.name>
2011-05-29 17:42:35 -0700

    handling multiple accepts

--- controllers/components/content_negotiation_component.php
+++ controllers/components/content_negotiation_component.php
@@ -16,26 +16,47 @@ class ContentNegotiationComponent extends Object {
             return;
         }
 
-        $responses = $this->responses;
+        foreach ($this->responses as $content_type => $callback) {
+            $pattern = '/^' . str_replace('/', '\\/', $content_type) . '/';
+            $responses[$pattern] = array('content_type' => $content_type, 'callback' => $callback);
+        }
 
         $this->nugget->post_request->last(function(NuggetRequest &$request, &$response) use ($responses){
             $accepts = array_map(function($element){
                 return trim($element);
             }, explode(',', $request->header("Accept")));
 
-            foreach ($responses as $content_type => $callback ) {
-                $pattern = '/^' . str_replace('/', '\\/', $content_type) . '/';
-                foreach ($accepts as $accept) {
+            usort($accepts, function($a, $b){
+                $a = explode(';', $a);
+                $b = explode(';', $b);
+
+                $q_a = array_key_exists('q', $a)
+                        ? (float)$a['q']
+                        : 0.0;
+
+                $q_b = array_key_exists('q', $b)
+                        ? (float)$b['q']
+                        : 0.0;
+
+                if ($q_a == $q_b)
+                    return 0;
+
+                return $q_a < $q_b ? -1 : 1;
+            });
+
+            foreach ($accepts as $accept) {
+                foreach ($responses as $pattern => $value) {
                     if (false == preg_match($pattern, $accept)) {
                         continue;
                     }
+                    $callback = $value['callback'];
                     $callback($request, $response);
-                    $response->content_type = $content_type;
+                    $response->content_type = $value['content_type'];
+
+                    return;
                }
             }
         });
     }
-
-    
 }
 ?>
--- features/content_negotiation.feature
+++ features/content_negotiation.feature
@@ -1,10 +1,17 @@
 Feature: Content Negotiation
 
-Scenario: Accepts
+Background:
     Given a nugget "Conneg"
         And the nugget is using component "ContentNegotiation"
-    And the "Accept" header is "application/json"
+
+
+Scenario: Accepts
+    Given the "Accept" header is "application/json"
     When I request "/conneg/simple"
     Then the "Content-type" response header should be "application/json"
         And the response body should be json
 
+Scenario: Multiple Accepts with quality
+    Given the "Accept" header is "application/json; q=0.1, text/html; q=1.0"
+    When I request "/conneg/simple"
+    Then the "Content-type" response header should be "text/html"
