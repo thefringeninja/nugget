9a6b164563e226c76333fa5ad63fb32d4a1ac848
Joao Braganca <joao.paul@braganca.name>
2011-08-11 16:43:24 -0700

    there really is no reason to support php 4.

--- libs/nugget.php
+++ libs/nugget.php
@@ -1,25 +1,16 @@
 <?php
 class Nugget {
-    private $nuggets = array();
+    private static $nuggets = array();
 
-    function &getInstance() {
-            static $instance = array();
-            if (!$instance) {
-                    $instance[0] =& new Nugget();
-            }
-            return $instance[0];
-    }
-
-    function load($nugget) {
-        $_this = &Nugget::getInstance();
-        if (false == isset($_this->nuggets[$nugget])) {
+    public static function load($nugget) {
+        if (false === isset(Nugget::$nuggets[$nugget])) {
             if (false == App::import('Controller', $nugget . 'Nugget')) {
                 return false;
             }
             $class = $nugget . 'NuggetController';
-            $_this->nuggets[$nugget] = new $class;
+            Nugget::$nuggets[$nugget] = new $class;
         }
-        return $_this->nuggets[$nugget];
+        return Nugget::$nuggets[$nugget];
     }
 }
 ?>
