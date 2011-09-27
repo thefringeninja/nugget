e7d18c85700cf1941606ac60222a17cb624673ee
jbraganca <joao.paul@braganca.name>
2011-09-26 19:26:57 -0700

    allowing caller to specify the kind of view that is rendered

--- libs/cake_view_nugget_response.php
+++ libs/cake_view_nugget_response.php
@@ -4,11 +4,13 @@ class CakeViewNuggetResponse extends NuggetResponse {
     function  __construct(array $params = array()) {
         $params = array_merge(array(
                                    'layout' => 'default',
-                                   'content_type' => 'text/html'
+                                   'content_type' => 'text/html',
+	                               'view_class' => 'View'
                               ), $params);
         parent::__construct($params);
         $this->renderCallback = function($model) use($params) {
-            $cake_view = new View($params['nugget']);
+	        $view_class = $params['view_class'];
+            $cake_view = new $view_class($params['nugget']);
             $cake_view->viewVars['model'] = $model;
             $cake_view->viewPath = null;
             echo $cake_view->render($params['view'], $params['layout']);
