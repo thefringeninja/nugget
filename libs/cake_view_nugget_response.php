3d5036f392b4b3f71ceb804dd048b6df17b12b09
thefringeninja <joao.paul@braganca.name>
2011-10-17 21:08:22 -0700

    so we can override this responses' fields in the pipeline

--- libs/cake_view_nugget_response.php
+++ libs/cake_view_nugget_response.php
@@ -1,20 +1,21 @@
 <?php
 App::import('Lib', 'Nugget.NuggetResponse');
 class CakeViewNuggetResponse extends NuggetResponse {
-    function  __construct(array $params = array()) {
-        $params = array_merge(array(
-                                   'layout' => 'default',
-                                   'content_type' => 'text/html',
-	                               'view_class' => 'View'
-                              ), $params);
-        parent::__construct($params);
-        $this->renderCallback = function($model) use($params) {
-	        $view_class = $params['view_class'];
-            $cake_view = new $view_class($params['nugget']);
-            $cake_view->viewVars['model'] = $model;
-            $cake_view->viewPath = null;
-            echo $cake_view->render($params['view'], $params['layout']);
-        };
-    }
+	function  __construct(array $params = array()) {
+		$params = array_merge(array(
+		                           'layout' => 'default',
+		                           'content_type' => 'text/html',
+		                           'view_class' => 'View'
+		                      ), $params);
+		parent::__construct($params);
+		$response = $this;
+		$this->renderCallback = function($model) use($response) {
+			$view_class = $response->view_class;
+			$cake_view = new $view_class($response->nugget);
+			$cake_view->viewVars['model'] = $model;
+			$cake_view->viewPath = null;
+			echo $cake_view->render($response->view, $response->layout);
+		};
+	}
 }
 ?>
